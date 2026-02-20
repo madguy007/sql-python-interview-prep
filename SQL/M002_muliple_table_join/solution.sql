bf94a02c82d593a53589e98d15bd9912e6cbb132
Madan Dahiphale <95565255+madguy007@users.noreply.github.com>
2026-02-20 09:51:52 +0530

    Implement INNER JOIN for orders and customers
    Added SQL query using INNER JOIN to retrieve order and customer details.

--- SQL/M002_muliple_table_join/solution.sql
+++ SQL/M002_muliple_table_join/solution.sql
@@ -1,1 +1,12 @@
+-- Solution using INNER JOIN (recommended modern syntax)
 
+SELECT o.ord_no,
+       c.cust_name,
+       o.customer_id,
+       o.salesman_id
+FROM orders o
+JOIN customer c
+  ON o.customer_id = c.customer_id
+JOIN salesman s
+  ON o.salesman_id = s.salesman_id
+WHERE c.city <> s.city;
