da576b0c01f4cb3bde197184950a987c8c0c3a07
Madan Dahiphale <95565255+madguy007@users.noreply.github.com>
2026-03-16 21:21:55 +0530

    Update solution.py

--- Python/Medium/M003_excel_column_number/solution.py
+++ Python/Medium/M003_excel_column_number/solution.py
@@ -10,3 +10,16 @@ def excel_col_num():
 
 
 print(excel_col_num())
+
+--- sencond method 
+
+letter = 'FXSHRXW'
+n = len(letter)
+val = 0
+mul = 0
+
+for i in range(n):
+  mul =  (26 ** (n-i-1))*(ord(letter[i])-64)
+  val += mul
+
+print(val)
