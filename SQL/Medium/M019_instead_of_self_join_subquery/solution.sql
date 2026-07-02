18f1b034dbddd750ef394b753fdd8a44389a4cda
Madan Dahiphale <95565255+madguy007@users.noreply.github.com>
2026-07-02 12:41:20 +0530

    Update solution.sql

--- SQL/Medium/M019_instead_of_self_join_subquery/solution.sql
+++ SQL/Medium/M019_instead_of_self_join_subquery/solution.sql
@@ -1,3 +1,6 @@
+
+### Mehtod 1 using group by and having 
+    
 SELECT 
     ROUND(SUM(tiv_2016), 2) AS tiv_2016
 FROM Insurance
@@ -13,3 +16,21 @@ AND (lat, lon) IN (
     GROUP BY lat, lon
     HAVING COUNT(*) = 1
 );
+
+
+### Method 2 using correlated subquery 
+    
+SELECT 
+  ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance AS i1
+WHERE (
+  SELECT COUNT(*) 
+  FROM Insurance AS i2 
+  WHERE i1.tiv_2015 = i2.tiv_2015
+) > 1
+AND (
+  SELECT COUNT(*) 
+  FROM Insurance AS i3 
+  WHERE i3.lat = i1.lat 
+    AND i3.lon = i1.lon
+) = 1;
